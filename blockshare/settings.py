28e1d544439acc43ae4d815b6bcf280e3deb0951
cponeill <caseyoneill78@hotmail.com>
2016-08-16 19:57:00 -0400

    minor settings changes

--- blockshare/settings.py
+++ blockshare/settings.py
@@ -108,4 +108,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = "blockshare/static"
 STATIC_URL = '/static/'
+
